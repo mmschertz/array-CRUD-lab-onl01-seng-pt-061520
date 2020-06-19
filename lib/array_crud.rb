b01fb636c56d8f5f5c4e5c32ff7383a8cf324d88
Megan Schertz <meganschertz@gmail.com>
2020-06-19 18:22:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["Cats", "Dogs"]
+  add_element_to_end_of_array = ["Cats", "Dogs"]
   add_element_to_end_of_array.push("Bunnies")
 end
 
