1610b43bc06d75f5d9744a0ef9c705f65ea7c44b
Megan Schertz <meganschertz@gmail.com>
2020-06-19 18:29:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-   ["array"]
-  add_element_to_end_of_array.push("arrays!")
-  puts add_element_to_start_of_array.inspect
+   array << element
 end
 
 def add_element_to_start_of_array(array, element)
