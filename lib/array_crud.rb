648a9538560cd4949c6c1c4cd324e37bbe8d7f5d
Megan Schertz <meganschertz@gmail.com>
2020-06-19 18:18:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   [1]
   add_element_to_end_of_array << 2
+  puts add_element_to_end_of_array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
